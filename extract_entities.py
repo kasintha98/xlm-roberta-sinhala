e9cd69897ef581a1797c7cbf8668992d28b99a87
Kasintha Kalhara <39892786+kasintha98@users.noreply.github.com>
2023-02-11 22:14:05 +0000

    cg

--- extract_entities.py
+++ extract_entities.py
@@ -1,5 +1,6 @@
 from simpletransformers.ner import NERModel
 from get_configs import Configs
+from fuzzysearch import find_near_matches
 
 label = Configs.get_labels()
 args = Configs.get_args()
@@ -15,5 +16,19 @@ class ExtractEntities:
         # prediction = [[{'ආසියානු': 'PERSON'}, {'ක්\u200dරිකට්': 'PERSON'}, {'ශුර': 'PERSON'}, {'ශ්\u200dරී':
         # 'PERSON'}, {'ලංකා': 'PERSON'}, {'කණ්ඩායම': 'PERSON'}, {'සහ': 'PERSON'}, {'සත්කාරක': 'PERSON'}, {'ඉන්දීය':
         # 'PERSON'}, {'කණ්ඩායම': 'DATE'}]]
-        entities = [{"text": [*ent][0], "entity": list(ent.values())[0], "value": [*ent][0]} for ent in prediction[0]]
+        # entities = [{"text": [*ent][0], "entity": list(ent.values())[0], "value": [*ent][0], "extractedBy": "xlm-roberta_model"} for ent in prediction[0]]
+        entities = []
+
+        for ent in prediction[0]:
+            start_and_end = ExtractEntities.get_start_and_end_char(text, [*ent][0])
+            if start_and_end:
+                entity = {"text": [*ent][0], "entity": list(ent.values())[0], "value": [*ent][0], "startChar": start_and_end[0].start,
+                                 "endChar": start_and_end[0].end, "extractedBy": "xlm-roberta_model"}
+                entities.append(entity)
+
         return entities
+
+    @staticmethod
+    def get_start_and_end_char(full_text, matched_text):
+        near_match = find_near_matches(matched_text, full_text, max_l_dist=1)
+        return near_match
